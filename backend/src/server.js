3b3ae6f87e5fc904b26dfe64b19b9564704dd211
Winston Pichardo <winston1420@hotmail.com>
2025-02-02 10:26:21 -0400

    Actualización de dependencias y correción de entry point

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,10 +18,7 @@ app.use(express.json());
 
 // Conectar a MongoDB
 mongoose
-.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+.connect(process.env.MONGO_URI)
 .then(() => console.log("Conectado a MongoDB"))
 .catch((err) => console.error("Error de conexión:", err));
 
