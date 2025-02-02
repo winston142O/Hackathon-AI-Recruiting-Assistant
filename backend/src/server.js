69d3bc0febdb6fbe3fb338d65fe07be46be83e1a
Winston Pichardo <winston1420@hotmail.com>
2025-02-02 10:17:59 -0400

    Inicialización del backend

---
+++ backend/src/server.js
@@ -0,0 +1,44 @@
+// Módulos y dependencias
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cors = require("cors");
+
+// Cargar variables de entorno
+dotenv.config();
+
+const app = express(); // Inicializar Express
+
+// Middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+// Conectar a MongoDB
+mongoose
+.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Conectado a MongoDB"))
+.catch((err) => console.error("Error de conexión:", err));
+
+
+// Rutas
+// const authRoutes = require("./routes/auth");
+// const privateRoutes = require("./routes/private");
+
+// app.use("/auth", authRoutes);
+// app.use("/private", privateRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API con Express y MongoDB en HTTP 🚀");
+});
+
+// Levantar servidor en un puerto local (por defecto 3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor HTTP corriendo en http://localhost:${PORT}`);
+});
